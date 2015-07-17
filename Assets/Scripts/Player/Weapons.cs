5c53fe0ec49054fce5ead23c9f1254eba1952e92
David Chaseling <davidchaseling@live.com.au>
2015-07-17 22:17:40 +1000

    Updated Gun

--- Assets/Scripts/Player/Weapons.cs
+++ Assets/Scripts/Player/Weapons.cs
@@ -4,52 +4,47 @@ using System.Collections;
 public class Weapons : MonoBehaviour {
 	
 	//the point we want to fire the bullet from
-	public Transform FiringPoint;
-	private GameObject currentType;
-	private GameObject selectedWeapon;
-	private GameObject selectedWType;
-
-	public Transform weaponSpot;
-	private GameObject WeaponType;
-	public GameObject Tree;
-	public GameObject Water;
-
-	//the bullet itself
-	private GameObject Bullet;
-	public GameObject TreeBullet;
-	public GameObject WaterBullet;
+	public Transform FiringPoint;	
 	//the camera
 	public Camera MainCamera;
 	//the point we want the bullet to travel to
 	public Vector3 TargetPoint;
-
 	private Vector3 point;
+
 	private bullet bullet; 
 
 	public float fireRate = 0.5f;
 	private float nextFire = 0.0f;
-
-
+	
 	private GameObject Gunshot;
 	private GameObject floor;
 	//how long the bullet can travel before detroying itself
-	private float WeaponRange = 335.0f;
+	private float WeaponRange = 30.0f;
 	
 	public float scaleAmount = 1.2501f;
+	
 
-	public GameObject frozenFloor;
-	public GameObject planet;
+	public int gunEnergy = 100;
+	private bool energy;
+	public GameObject Bullet;
 
-	public float gunEnergy = 10.0f;
+	private bool scaling = false;
 
 	void Start(){
-		SelectedWeapon (Tree, TreeBullet);
 		point = new Vector3 (0.5f, 0.5f, 0f);
+		//FiringPoint.parent = GameObject.Find ("space-gun").transform;
 	}
 
 	void Update(){
-		if (gunEnergy < 10) {
-			gunEnergy += 0.025f;
+		//Recharges gun;
+		if (gunEnergy < 100) {
+			gunEnergy += 1;
+		}
+		//Checks gun has energy
+		if (gunEnergy < 0) {
+			energy = false;
+		} else {
+			energy = true;
 		}
 		//the beam comes from the camera itself to the tagetpoint
 		Ray beam = MainCamera.ViewportPointToRay(point);
@@ -59,31 +54,92 @@ public class Weapons : MonoBehaviour {
 			//tells the beam this is the target point
 			TargetPoint=hit.point;
 			//draws the beam in the senceview so you can see it
-		
 			Debug.DrawLine (FiringPoint.position, hit.point);      
 		}
-		//Shoot Press
-		if(Input.GetKeyDown(KeyCode.Mouse0) && Time.time > nextFire){
+		//Handles user inputs
+		userInputs ();
+	}
+
+	/*Handles user inputs*/
+	void userInputs(){
+		//Left Shoot Press
+		if(Input.GetButtonDown("Fire1") && Time.time > nextFire && energy){
+			gunEnergy -= 10;
 			//updates nextFire
 			nextFire = Time.time + fireRate;
 			//instantiates bullet/object
 			Gunshot = Instantiate(Bullet,FiringPoint.position,FiringPoint.rotation) as GameObject;
-
-			Gunshot.GetComponent<bullet>().FiringPoint = FiringPoint;
-			//parents object
-			Gunshot.transform.parent = GameObject.Find("space-gun").transform;
-			//destroys object
+			Gunshot.GetComponent<bullet>().Point = TargetPoint;
+			Gunshot.GetComponent<bullet>().Shoot = true;
 			Destroy (Gunshot, WeaponRange);
-
-
-		}
-		//Shoot Hold
-		if (Input.GetKey (KeyCode.Mouse0)) {
-			if (hit.collider.tag == "water"){
-				ScaleObject(hit.collider.gameObject);
-			}
+			Destroy (Gunshot.GetComponent<bullet>().Target, WeaponRange);
 		}
 
+		//Right Button Press
+		if (Input.GetButtonDown ("Fire2") && Time.time > nextFire && energy && scaling == false) {
+			scaling = true;
+			//instantiates bullet/object
+			Gunshot = Instantiate(Bullet,FiringPoint.position,FiringPoint.rotation) as GameObject;
+			Gunshot.transform.parent = FiringPoint;
+			ScaleObject(Gunshot.transform);
+		}
+		//Right Shoot Hold
+		if (Input.GetButton ("Fire2") && energy) {
+			ScaleObject(Gunshot.transform);
+		}
+		
+		//Right Shoot Realease
+		if (Input.GetButtonUp ("Fire2") || (!energy)) {
+			scaling = false;
+			//tells object where to go
+			nextFire = Time.time + fireRate;
+			Gunshot.transform.parent = GameObject.Find("Planet").transform;
+			scaling = false;
+			Gunshot.GetComponent<bullet>().Point = TargetPoint;
+			Gunshot.GetComponent<bullet>().Shoot = true;
+			Destroy (Gunshot, WeaponRange);
+			Destroy (Gunshot.GetComponent<bullet>().Target, WeaponRange);
+		}
+		
+		/*
+		if (hit.collider.tag == "frozen" && gunEnergy > 0) {
+			gunEnergy -= 1;
+			ScaleObject(hit.collider.gameObject);
+		}*/
+	}
+	void instantiateObject(){
+		nextFire = Time.time + fireRate;
+		//instantiates bullet/object
+		Gunshot = Instantiate(Bullet,FiringPoint.position,FiringPoint.rotation) as GameObject;
+		Gunshot.GetComponent<bullet>().Point = TargetPoint;
+		Gunshot.GetComponent<bullet>().Shoot = true;
+		Destroy (Gunshot, WeaponRange);
+		Destroy (Gunshot.GetComponent<bullet>().Target, WeaponRange);
+	}
+
+	/*Scales Bullet*/
+	void ScaleObject(Transform scaleObject){
+		print ("scaling");
+		gunEnergy -= 1;
+		if (scaleObject.localScale.magnitude < 100) {
+			//scales Object
+			scaleObject.localScale = new Vector3 (scaleObject.localScale.x * scaleAmount, scaleObject.localScale.y * scaleAmount, scaleObject.localScale.z * scaleAmount);
+			scaleObject.transform.position += 0.015f * FiringPoint.right;
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
 		//forzen floor test
 		if (Input.GetKey (KeyCode.Mouse1)) {
 			if (hit.collider.tag == "water" && gunEnergy > 0){
@@ -99,42 +155,10 @@ public class Weapons : MonoBehaviour {
 			}			
 		}
 
-		//Shoot Realease
-		if (Input.GetKeyUp (KeyCode.Mouse0)) {
-			//tells object where to go
-			Gunshot.GetComponent<bullet>().Shoot = true;
-			Gunshot.GetComponent<bullet>().Point = TargetPoint;
+ //Shoot Hold
+		if (Input.GetButton ("Fire1")) {
+			if (hit.collider.tag == "water"){
+				ScaleObject(hit.collider.gameObject);
+			}
 		}
-
-		//Switches weapons
-		if (Input.GetKeyDown (KeyCode.Q)) {
-			SelectedWeapon(Tree, TreeBullet);
-		}
-		if (Input.GetKeyDown (KeyCode.E)) {
-			SelectedWeapon(Water, WaterBullet);
-		}
-
-		if (hit.collider.tag == "frozen" && gunEnergy > 0) {
-			gunEnergy -= 0.1f;
-			ScaleObject(hit.collider.gameObject);
-		}
-
-	}
-
-	void SelectedWeapon(GameObject selectedType, GameObject selectedWeapon){
-		if(!(currentType == selectedType)){
-			Destroy (currentType);
-			currentType = Instantiate (selectedType, weaponSpot.position, weaponSpot.rotation) as GameObject;
-			currentType.transform.parent = MainCamera.transform;
-			Bullet = selectedWeapon;
-		}
-	}
-
-	void ScaleObject(GameObject scaleObject){
-		if (scaleObject.transform.localScale.magnitude < 3) {
-			//adsadyh
-			scaleObject.transform.localScale = new Vector3 (scaleObject.transform.localScale.x * scaleAmount, scaleObject.transform.localScale.y * scaleAmount, scaleObject.transform.localScale.z * scaleAmount);
-		}
-	}
-}
-
+ */
